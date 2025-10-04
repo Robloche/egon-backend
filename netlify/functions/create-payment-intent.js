bda7415f7e2c16a4da1ff7dabb673ce39939c8f7
Rodolphe Breton <rodolphe.breton@netgem.com>
2025-10-04 10:06:43 +0200

    Initial commit

---
+++ netlify/functions/create-payment-intent.js
@@ -0,0 +1,26 @@
+/* @flow */
+
+// Stripe private key
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY_TEST);
+
+exports.handler = async(event) => {
+  console.log(event);
+
+  try {
+    const intent = await stripe.paymentIntents.create({
+      amount: 2000, // En centimes (20,00 € ici)
+      automatic_payment_methods: { enabled: true },
+      currency: 'eur',
+    });
+
+    return {
+      body: JSON.stringify({ clientSecret: intent.client_secret }),
+      statusCode: 200,
+    };
+  } catch (err) {
+    return {
+      body: JSON.stringify({ error: err.message }),
+      statusCode: 500,
+    };
+  }
+};
